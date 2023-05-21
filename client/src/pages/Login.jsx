89500f14eef3ed4973a894be00023ef0b460c8f5
Rasmus Olofsson Burman <rasmus_ob@outlook.com>
2023-05-21 16:48:54 +0200

    Added more functionality to signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,21 @@
-import Form from '../components/Form';
 import '../css/page.css';
+import '../css/form.css';
 
 function Login() {
 	return (
 		<>
 			<h1 className='page-title'>Login</h1>
-			<Form />
+			<form className='form'>
+				<input type='text' placeholder='Username' className='form-input' />
+				<input type='email' placeholder='Email' className='form-input' />
+				<input type='password' placeholder='Password' className='form-input' />
+				<input
+					type='password'
+					placeholder='Repeat Password'
+					className='form-input'
+				/>
+				<input type='submit' value='Submit' className='form-btn' />
+			</form>
 		</>
 	);
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,13 +1,66 @@
-import Form from '../components/Form';
+import { useState } from 'react';
 import '../css/page.css';
 
 function Signup() {
+	const dataTemplate = {
+		username: '',
+		email: '',
+		password: '',
+		passwordRepeat: '',
+	};
+
+	const [data, setData] = useState(dataTemplate);
+
+	const updateData = (inputEvent) => {
+		const inputValue = inputEvent.target.value;
+		const key = inputEvent.target.id;
+
+		setData((prev) => ({ ...prev, [key]: inputValue }));
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		// write to database
+	};
+
 	return (
 		<>
 			<h1 className='page-title'>Signup</h1>
-			<div className='container'>
-				<Form />
-			</div>
+			<form className='form' onSubmit={(e) => submitHandler(e)}>
+				<input
+					type='text'
+					placeholder='Username'
+					className='form-input'
+					id='username'
+					onChange={(e) => updateData(e)}
+					value={data.username}
+				/>
+				<input
+					type='email'
+					placeholder='Email'
+					className='form-input'
+					id='email'
+					onChange={(e) => updateData(e)}
+					value={data.email}
+				/>
+				<input
+					type='password'
+					placeholder='Password'
+					className='form-input'
+					id='password'
+					onChange={(e) => updateData(e)}
+					value={data.password}
+				/>
+				<input
+					type='password'
+					placeholder='Repeat Password'
+					className='form-input'
+					id='passwordRepeat'
+					onChange={(e) => updateData(e)}
+					value={data.passwordRepeat}
+				/>
+				<input type='submit' value='Submit' className='form-btn' />
+			</form>
 		</>
 	);
 }
