727522c1358e09f95ffec565f31e5bfdc9f47e01
Rasmus Olofsson Burman <rasmus_ob@outlook.com>
2023-05-22 13:07:00 +0200

    Updated the Dashboard UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,97 @@
+import { Line } from 'react-chartjs-2';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js';
+
+import '../css/dashboard.css';
+
 function Dashboard() {
+	ChartJS.register(
+		CategoryScale,
+		LinearScale,
+		PointElement,
+		LineElement,
+		Title,
+		Tooltip,
+		Legend
+	);
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Most recent table',
+			},
+		},
+	};
+
+	const labels = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+	];
+
+	const exampleApiData = {
+		labels,
+		datasets: [
+			{
+				label: 'Bench',
+				data: labels.map(() => Math.random(0, 100)),
+				borderColor: 'rgb(255, 99, 132)',
+				backgroundColor: 'rgba(255, 99, 132, 0.5)',
+			},
+			{
+				label: 'Squat',
+				data: labels.map(() => Math.random(0, 100)),
+				borderColor: '#000FFF',
+				backgroundColor: '#0000FF',
+			},
+		],
+	};
+
 	return (
 		<>
-			<h1>Dashboard</h1>
+			<h1 className='page-title'>Dashboard</h1>
+			<div className='dashboard'>
+				<div className='recent-tables'>
+					<li>
+						<button>Workouts</button>
+					</li>
+					<li>
+						<button>Weight Tracking</button>
+					</li>
+					<li>
+						<button>Water drinking</button>
+					</li>
+					<li>
+						<button>Money Habits</button>
+					</li>
+					<li>
+						<button>Screen Time</button>
+					</li>
+					<li>
+						<button>Show more</button>
+					</li>
+				</div>
+				<div className='summary-chart'>
+					<Line options={options} data={exampleApiData} />
+				</div>
+			</div>
 		</>
 	);
 }
