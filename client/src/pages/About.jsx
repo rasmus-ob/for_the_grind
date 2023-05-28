e586fe6f21e918ed158537514f12634ab51fb026
Rasmus Olofsson Burman <rasmus_ob@outlook.com>
2023-05-28 11:29:52 +0200

    Added information to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,37 @@
+import '../css/about.css';
+import '../css/page.css';
+
 function About() {
 	return (
 		<>
-			<h1>About</h1>
+			<div className='about page-container'>
+				<section className='section quote'>
+					Let us manage the statistics so that you can focus on what is
+					significant, the grind.
+				</section>
+				<section className='section introduction'>
+					For the grind is all about making the grind as easy as possible for
+					you so you can become more successful, in less time, with less hassle.
+				</section>
+				<section className='section'>
+					The statistics are significant to be on top of because they can make
+					the difference between you making it or face-planting with your head
+					first in the ground. Contemplate this. Who do you think will be more
+					successful? The person who just free balls everything or the person
+					that takes notes and peeks at the data, too consistently improve?
+					Probably the last option right?
+				</section>
+				<section className='section'>
+					We make it effortless to record and understand statistics, through
+					graphs and data. The diagrams make it easy to see where you lack and
+					recognize where you need to put in more effort before it harms your
+					progress.
+				</section>
+				<section className='section'>
+					When you can take part in the statistics with ease, you are much more
+					likely to keep on grinding.
+				</section>
+			</div>
 		</>
 	);
 }
